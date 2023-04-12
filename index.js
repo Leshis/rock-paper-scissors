b66797bb279c4dad34f77a27faa7ff3d8f76f54d
Leshis <94430516+Leshis@users.noreply.github.com>
2023-04-12 09:42:32 +0100

    Consolidated the playRound function

--- index.js
+++ index.js
@@ -1,65 +1,44 @@
 const choice = ["Rock", "Paper", "Scissors"];
-const playerChoice = "rock"
+const tie = "The result is a tie!";
+const win = "You win!";
+const lose = "Computer wins";
 let playerScore = 0;
 let computerScore = 0;
 
-function getComputerChoice(){
-    return (choice[Math.floor(Math.random()*3)]);
+function getComputerChoice() {
+    return (choice[Math.floor(Math.random() * choice.length)]);
 }
 
-function getPlayerChoice(){
+function getPlayerChoice() {
     return window.prompt("Enter your selection");
 }
 
-function playRound(playerChoice, computerChoice){
-
-    if(playerChoice.toLowerCase() === computerChoice.toLowerCase()){
-        return "The result is a tie!"
-    } 
-
-    if(playerChoice.toLowerCase() === "rock"){
-        if(computerChoice.toLowerCase() === "scissors"){
-            playerScore++;
-            return "You win!";
-        } else{
-            computerScore++;
-            return "Computer wins!";
-        }
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice.toLowerCase() === computerChoice.toLowerCase()) {
+        return tie;
     }
-
-    if(playerChoice.toLowerCase() === "scissors"){
-        if(computerChoice.toLowerCase() === "paper"){
-            playerScore++;
-            return "You win!";
-        } else{
-            computerScore++;
-            return "Computer wins";
-        }
+    if ((playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "scissors")
+        || (playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "paper")
+        || (playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "rock")) {
+        playerScore++;
+        return win;
     }
-
-    if(playerChoice.toLowerCase() === "paper"){
-        if(computerChoice.toLowerCase() === "rock"){
-            playerScore++;
-            return "You win!";
-        } else{
-            computerScore++;
-            return "Computer wins";
-        }
-    }
+    computerScore++;
+    return lose;
 }
 
-function checkWinner(){
-    if(playerScore > computerScore){
+function checkWinner() {
+    if (playerScore > computerScore) {
         return "You win overall!";
-    }else if(computerScore > playerScore){
+    } else if (computerScore > playerScore) {
         return "Computer wins overall!";
-    }else{
+    } else {
         return "Tie game!";
     }
 }
 
-function game(){
-    for(let i = 0; i < 5; i++){
+function game() {
+    for (let i = 0; i < 5; i++) {
         console.log(playRound(getPlayerChoice(), getComputerChoice()));
     }
     console.log(checkWinner());
